0ce4a2f0d999fd2d89992a3e0b216448b57a87a9
Valeed Naeem <valeednaeem@gmail.com>
2026-01-30 16:26:23 +0500

    Next Config File
    Next Config File

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  // reactCompiler: true,
-  // reactStrictMode: true,
+  reactCompiler: true,
+  reactStrictMode: true,
+  output: "export",
 };
 
 export default nextConfig;
