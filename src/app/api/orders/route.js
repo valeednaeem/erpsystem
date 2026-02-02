dbb611d600c353ba926a45b769b7e3bd4cd32757
Valeed Naeem <valeednaeem@gmail.com>
2026-02-02 16:05:40 +0500

    order api
    new file

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -1,7 +1,11 @@
+
 import { NextResponse } from "next/server";
-import db from "../../../lib/db";
+import { execute } from "@/lib/db";
 import { requirePermission } from "@/lib/apiGuard";
 import { logActivity } from "@/lib/activityLogger";
+
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
 
 
 // GET: list sales orders
