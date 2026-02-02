a342f2dbbc8778537619773601c851f4cdf8ce07
Valeed Naeem <valeednaeem@gmail.com>
2026-02-02 13:51:11 +0500

    Refactor import paths in login route
    @/*

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,4 +1,4 @@
-import db from "../../../../lib/db";
+import db from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
@@ -15,3 +15,4 @@ export async function POST(req) {
 
   return NextResponse.json({ user: rows[0] });
 }
+
