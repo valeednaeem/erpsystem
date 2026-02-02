34f64394f0f183aa1df56d4e2746f5fae9f39b3b
Valeed Naeem <valeednaeem@gmail.com>
2026-02-02 16:22:13 +0500

    inventory file updated
    export const runtime = "nodejs";
    export const dynamic = "force-dynamic";

--- src/app/api/inventory/route.js
+++ src/app/api/inventory/route.js
@@ -1,6 +1,9 @@
 import db from "../../../lib/db";
 import { NextResponse } from "next/server";
 
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
 
 export async function GET() {
   const [rows] = await db.query("SELECT * FROM inventory");
